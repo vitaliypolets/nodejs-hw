cbd704322e2f4f1f503e68f9033f61ae26d533b1
Vitaliy <vspolets@gmail.com>
2026-06-28 14:12:31 +0300

    Add express server for notes homework

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
