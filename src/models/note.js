1b7f1b1bf76baf5e65769a77cdec0cbf92cfe767
Vitaliy <vspolets@gmail.com>
2026-07-17 23:09:57 +0300

    Fix tag schema index

--- src/models/note.js
+++ src/models/note.js
@@ -22,7 +22,6 @@ const noteSchema = new Schema(
       type: String,
       enum: TAGS,
       default: 'Todo',
-      index: true,
     },
   },
   {
@@ -30,5 +29,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ tag: 1 });
 
 export const Note = model('Note', noteSchema, 'notes');
